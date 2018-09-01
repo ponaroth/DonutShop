98a16519685a1b6e75bd35e86963698cd59851aa
Ponaroth Eab <ronadean@hotmail.com>
2018-08-31 17:47:20 -0700

    Add codes

--- Driver.py
+++ Driver.py
@@ -7,4 +7,33 @@ twist = Product("twists and bars", 1.10)
 
 print(glazed.name, glazed.price)
 
-#add product prices
+#A list of Products
+cart = []
+
+
+#add a product to cart
+def add(productA):
+    cart.append(productA)
+
+
+def getTotal():
+    totalPrice = 0
+
+    if not cart:
+        print("cart is empty")
+
+    else:
+        for item in cart:
+            totalPrice = totalPrice + item.price
+
+    return totalPrice
+
+cart.append(glazed)
+cart.append(glazed)
+cart.append(glazed)
+cart.append(twist)
+cart.append(twist)
+
+
+
+print("the total is ", '${:,.2f}'.format(getTotal()))
