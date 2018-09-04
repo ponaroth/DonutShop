85a89aba0e2b486a334d13aa2afd19a46381b544
Ponaroth Eab <ronadean@hotmail.com>
2018-09-04 00:33:26 -0700

    fixing error in getTotal()

--- MenuGUI.py
+++ MenuGUI.py
@@ -8,11 +8,15 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 
 
-cart = []
 # cart
+cart = []
+
+
 
 # menu GUI
 def app():
+
+
     myApp = QApplication(sys.argv)
     window = QWidget()
     window.setWindowTitle("DonutMenu")
@@ -28,12 +32,14 @@ def app():
     numOfItemInRow = 0
     column = 0
 
+    buttonList = []
+    i = 0
     for product in productList:
         print(product.name, "/n", product.price )
-        name = product.name + "\n" + product.price
+        name = product.name + "\n" + str(product.price)
         btn = QtWidgets.QPushButton(window)
         btn.setText(name)
-        btn.clicked.connect(lambda: on_click(cart_lb))
+        btn.clicked.connect(lambda: addToCart(product))
         btn.setGeometry(geo[0], geo[1], geo[2], geo[3])
 
         if numOfItemInRow < 4:
@@ -48,30 +54,25 @@ def app():
 
     window.setGeometry(400, 100, 600, 600)
 
-    @pyqtSlot()
-    def on_click(cart_lb):
-        cart_lb.setText("Cart total: " + product.price)
-        cart_lb.adjustSize()
-
     # add a product to cart
-    def add(product):
+    @pyqtSlot()
+    def addToCart(product):
         cart.append(product)
-        print(product.price)
 
-        cart_lb.setText("Cart total: " + product.price)
-        print(getTotal())
+        cart_lb.setText("Cart total: " + str(getTotal()))
+        cart_lb.adjustSize()
 
 
     def getTotal():
         totalPrice = 0
+        print(type(totalPrice))
 
-        if not cart:
-            print("cart is empty")
+        for item in cart:
+            print(type(item.price))
+            totalPrice = totalPrice + item.price
+            print("2 total " + totalPrice)
 
-        else:
-            for item in cart:
-                totalPrice = totalPrice + item.price
-
+        print(totalPrice)
         return totalPrice
 
     window.show()
--- ProductList.py
+++ ProductList.py
@@ -2,12 +2,12 @@ from Product import Product
 
 productList = []
 
-productList.append(Product("Twist", "1.10"))
-productList.append(Product("croissant", "1.75"))
-productList.append(Product("Muffin", "2.00"))
-productList.append(Product("cookies", "1.00"))
-productList.append(Product("Coffee", "2.00"))
-productList.append(Product("Can Soda", "1.00"))
-productList.append(Product("Milk", "1.50"))
-productList.append(Product("Juice Box", "2.00"))
-productList.append(Product("smoothies", "2.50"))
+productList.append(Product("Twist", 1.10))
+productList.append(Product("croissant", 1.75))
+productList.append(Product("Muffin", 2.00))
+productList.append(Product("cookies", 1.00))
+productList.append(Product("Coffee", 2.00))
+productList.append(Product("Can Soda", 1.00))
+productList.append(Product("Milk", 1.50))
+productList.append(Product("Juice Box", 2.00))
+productList.append(Product("smoothies", 2.50))
